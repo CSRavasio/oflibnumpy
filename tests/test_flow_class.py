fd7395c578848628bebb0bc234aa37ea732b558e
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-03-09 18:25:54 +0000

    test_flow_class: test_from_transforms added

--- tests/test_flow_class.py
+++ tests/test_flow_class.py
@@ -29,6 +29,48 @@ class TestFlow(unittest.TestCase):
         self.assertIsNone(np.testing.assert_allclose(flow.vecs[199, 10], [-74.5, 19.9622148361]))
         self.assertIsNone(np.testing.assert_equal(flow.shape[:2], dims))
 
+    def test_from_transforms(self):
+        dims = [200, 300]
+        transforms = [['rotation', 10, 50, -30]]
+        flow = Flow.from_transforms(transforms, dims)
+        self.assertIsNone(np.testing.assert_array_almost_equal(flow.vecs[50, 10], [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[50, 299], [38.7186583063, 144.5]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[199, 10], [-74.5, 19.9622148361]))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[:2], dims))
+        self.assertEqual(flow.ref, 't')
+
+        transforms = [['scaling', 20, 30, 2]]
+        flow = Flow.from_transforms(transforms, dims, 's')
+        self.assertIsNone(np.testing.assert_array_almost_equal(flow.vecs[30, 20], [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[30, 70], [50, 0]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[80, 20], [0, 50]))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[:2], dims))
+        self.assertEqual(flow.ref, 's')
+
+        transforms = [
+            ['translation', -20, -30],
+            ['scaling', 0, 0, 2],
+            ['translation', 20, 30]
+        ]
+        flow = Flow.from_transforms(transforms, dims, 's')
+        self.assertIsNone(np.testing.assert_array_almost_equal(flow.vecs[30, 20], [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[30, 70], [50, 0]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[80, 20], [0, 50]))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[:2], dims))
+        self.assertEqual(flow.ref, 's')
+
+        transforms = [
+            ['translation', -10, -50],
+            ['rotation', 0, 0, -30],
+            ['translation', 10, 50]
+        ]
+        flow = Flow.from_transforms(transforms, dims, 't')
+        self.assertIsNone(np.testing.assert_array_almost_equal(flow.vecs[50, 10], [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[50, 299], [38.7186583063, 144.5]))
+        self.assertIsNone(np.testing.assert_allclose(flow.vecs[199, 10], [-74.5, 19.9622148361]))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[:2], dims))
+        self.assertEqual(flow.ref, 't')
+
 
 if __name__ == '__main__':
     unittest.main()
