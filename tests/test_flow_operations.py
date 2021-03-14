f2fa08bd57e533a2285544530356964859c8b4bd
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-03-14 17:15:27 +0000

    Document headers added

--- tests/test_flow_operations.py
+++ tests/test_flow_operations.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright: 2021, Claudio S. Ravasio
+# License: LGPL 2.1
+# Author: Claudio S. Ravasio, PhD student at University College London (UCl), supervised by:
+#   Dr Christos Bergeles, PI of the Robotics and Vision in Medicine (RViM) lab in the School of Biomedical Engineering &
+#       Imaging Sciences (BMEIS) at King's College London (KCL)
+#   Prof Lyndon Da Cruz, consultant ophthalmic surgeon, Moorfields Eye Hospital, London UK
+#
+# This file is part of flowlib
+
 import unittest
 import numpy as np
 import cv2
