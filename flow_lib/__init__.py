10dcc13693936b84bbb4f63becbc590f872af5a8
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-04-01 12:09:47 +0100

    __init__ header and content added

---
+++ flow_lib/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright: 2021, Claudio S. Ravasio
+# License: LGPL 2.1
+# Author: Claudio S. Ravasio, PhD student at University College London (UCl), supervised by:
+#   Dr Christos Bergeles, PI of the Robotics and Vision in Medicine (RViM) lab in the School of Biomedical Engineering &
+#       Imaging Sciences (BMEIS) at King's College London (KCL)
+#   Prof Lyndon Da Cruz, consultant ophthalmic surgeon, Moorfields Eye Hospital, London UK
+#
+# This file is part of oflibnumpy
+
+from .flow_class import Flow
+from .flow_operations import *
