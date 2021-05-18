aa424ef390afd201ac11363912097ac1d1fe8771
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-05-18 11:24:40 +0100

    setup.py updated, added all necessary arguments

--- setup.py
+++ setup.py
@@ -14,18 +14,44 @@
 from setuptools import setup
 
 
+with open("README.rst", "r") as readme_file:
+    long_description = readme_file.read()
+
 setup(
-      name='oflibnumpy',
-      version='1.0',
-      description='Optical flow library using a custom flow class based on NumPy arrays',
-      author='Claudio Ravasio',
-      author_email='claudio.s.ravasio@gmail.com',
-      packages=['oflibnumpy'],
-      install_requires=[
-            'numpy',
-            'opencv-python',
-            'scipy',
-            'typing~=3.7.4.3',
-            'sphinx~=3.5.2'
-      ]
+    name='oflibnumpy',
+    version='1.0.0',
+    description='Optical flow library using a custom flow class based on NumPy arrays',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/RViMLab/oflibnumpy',
+    author='Claudio S. Ravasio',
+    author_email='claudio.s.ravasio@gmail.com',
+    license='MIT',
+    classifiers=[
+      # How mature is this project? Common values are
+      #   3 - Alpha
+      #   4 - Beta
+      #   5 - Production/Stable
+      'Development Status :: 5 - Production/Stable',
+
+      # Pick your license as you wish (should match "license" above)
+      'License :: OSI Approved :: MIT License',
+
+      # Specify the Python versions you support here. In particular, ensure
+      # that you indicate whether you support Python 2, Python 3 or both.
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='optical flow, flow, flow field, flow composition, flow combination, flow visualisation',
+    project_urls={
+        'Documentation': 'https://readthedocs.org',
+        'Source': 'https://github.com/RViMLab/oflibnumpy',
+        'Tracker': 'https://github.com/RViMLab/oflibnumpy/issues',
+    },
+    packages=['oflibnumpy'],
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'scipy',
+    ],
+    python_requires='>=3.7',
 )
