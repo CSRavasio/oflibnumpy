8d6880d5dc87c6192410de271632bd5b0525ef90
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-11-12 17:49:53 +0000

    all missing tests for test_flow_operations added

--- tests/test_flow_operations.py
+++ tests/test_flow_operations.py
@@ -15,7 +15,9 @@ import unittest
 import cv2
 import numpy as np
 from oflibnumpy.flow_class import Flow
-from oflibnumpy.flow_operations import combine_flows, switch_flow_ref, invert_flow
+from oflibnumpy.flow_operations import combine_flows, switch_flow_ref, invert_flow, valid_target, valid_source, \
+    get_flow_padding, get_flow_matrix, visualise_flow, visualise_flow_arrows
+from oflibnumpy.utils import matrix_from_transforms
 
 
 class TestFlowOperations(unittest.TestCase):
@@ -84,6 +86,59 @@ class TestFlowOperations(unittest.TestCase):
         self.assertIsNone(np.testing.assert_equal(flow_s.invert('t').vecs, invert_flow(flow_s.vecs, 's', 't')))
         self.assertIsNone(np.testing.assert_equal(flow_t.invert().vecs, invert_flow(flow_t.vecs, 't')))
         self.assertIsNone(np.testing.assert_equal(flow_t.invert('s').vecs, invert_flow(flow_t.vecs, 't', 's')))
+
+    def test_valid_target(self):
+        transforms = [['rotation', 0, 0, 45]]
+        shape = (7, 7)
+        f_s = Flow.from_transforms(transforms, shape, 's')
+        f_t = Flow.from_transforms(transforms, shape, 't')
+        self.assertIsNone(np.testing.assert_equal(f_s.valid_target(), valid_target(f_s.vecs, 's')))
+        self.assertIsNone(np.testing.assert_equal(f_t.valid_target(), valid_target(f_t.vecs, 't')))
+
+    def test_valid_source(self):
+        transforms = [['rotation', 0, 0, 45]]
+        shape = (7, 7)
+        f_s = Flow.from_transforms(transforms, shape, 's')
+        f_t = Flow.from_transforms(transforms, shape, 't')
+        self.assertIsNone(np.testing.assert_equal(f_s.valid_source(), valid_source(f_s.vecs, 's')))
+        self.assertIsNone(np.testing.assert_equal(f_t.valid_source(), valid_source(f_t.vecs, 't')))
+
+    def test_get_flow_padding(self):
+        transforms = [['rotation', 0, 0, 45]]
+        shape = (7, 7)
+        f_s = Flow.from_transforms(transforms, shape, 's')
+        f_t = Flow.from_transforms(transforms, shape, 't')
+        self.assertIsNone(np.testing.assert_equal(f_s.get_padding(), get_flow_padding(f_s.vecs, 's')))
+        self.assertIsNone(np.testing.assert_equal(f_t.get_padding(), get_flow_padding(f_t.vecs, 't')))
+
+    def test_get_flow_matrix(self):
+        # Partial affine transform, test reconstruction with all methods
+        transforms = [
+            ['translation', 20, 10],
+            ['rotation', 200, 200, 30],
+            ['scaling', 100, 100, 1.1]
+        ]
+        matrix = matrix_from_transforms(transforms)
+        flow_s = Flow.from_matrix(matrix, (1000, 2000), 's')
+        flow_t = Flow.from_matrix(matrix, (1000, 2000), 't')
+        m_s_f = flow_s.matrix(dof=4, method='ransac')
+        m_t_f = flow_t.matrix(dof=4, method='ransac')
+        m_s = get_flow_matrix(flow_s.vecs, 's', dof=4, method='ransac')
+        m_t = get_flow_matrix(flow_t.vecs, 't', dof=4, method='ransac')
+        self.assertIsNone(np.testing.assert_allclose(m_s_f, m_s))
+        self.assertIsNone(np.testing.assert_allclose(m_t_f, m_t))
+
+    def test_visualise_flow(self):
+        flow = Flow.from_transforms([['translation', 1, 0]], [200, 300])
+        self.assertIsNone(np.testing.assert_equal(flow.visualise('bgr'), visualise_flow(flow.vecs, 'bgr')))
+        self.assertIsNone(np.testing.assert_equal(flow.visualise('rgb'), visualise_flow(flow.vecs, 'rgb')))
+        self.assertIsNone(np.testing.assert_equal(flow.visualise('hsv'), visualise_flow(flow.vecs, 'hsv')))
+
+    def test_visualise_flow_arrows(self):
+        for ref in ['s', 't']:
+            flow = Flow.from_transforms([['rotation', 10, 10, 30]], [20, 20], ref)
+            self.assertIsNone(np.testing.assert_equal(flow.visualise_arrows(),
+                                                      visualise_flow_arrows(flow.vecs, ref)))
 
 
 if __name__ == '__main__':
