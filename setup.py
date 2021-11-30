a0dab933b5e7edcfb385944e5d43186c5b0591c8
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-11-30 17:53:35 +0000

    setup.py: version number updated to 1.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.rst", "r") as readme_file:
 
 setup(
     name='oflibnumpy',
-    version='1.1.0',
+    version='1.1.1',
     description='Optical flow library using a custom flow class based on NumPy arrays',
     long_description=long_description,
     long_description_content_type='text/x-rst',
